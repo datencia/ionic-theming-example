4b71eeb330267553b961e8154715488d23472809
David Atencia <david.atencia@gmail.com>
2017-08-26 11:53:03 +0200

    apply the default theme on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { SettingsProvider } from './../providers/settings/settings';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:any = TabsPage;
+  selectedTheme: String;
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private settings: SettingsProvider,
+  ) {
+    this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +28,5 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
 }
