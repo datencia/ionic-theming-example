cca92c423b4bfd25acc94015a48706696ad9001a
David Atencia <david.atencia@gmail.com>
2017-08-26 12:08:43 +0200

    change between the light & dark theme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { SettingsProvider } from './../../providers/settings/settings';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  selectedTheme: String;
 
+  constructor(
+    private navCtrl: NavController,
+    private settings: SettingsProvider
+  ) {
+    this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
+  }
+
+  toggleAppTheme() {
+    if (this.selectedTheme === 'dark-theme') {
+      this.settings.setActiveTheme('light-theme');
+    } else {
+      this.settings.setActiveTheme('dark-theme');
+    }
   }
 
 }
